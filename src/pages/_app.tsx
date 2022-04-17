6cdb3c82d0765dd401c67a62d7ca75cd14b17c80
Bruno Domingues <brunood07@gmail.com>
2022-04-17 19:05:56 -0300

    feat: axios and pre config started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,12 @@ import '@fontsource/roboto';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { theme } from '../src/theme';
+import { theme } from '../theme';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider theme={theme} resetCSS>
     <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     </Head>
     <Component {...pageProps} />
   </ChakraProvider>
